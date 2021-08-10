3cc2483ed1bbc22deb022d5e5a397a7d94597129
lizapincsak <liza.pincsak@gmail.com>
2021-08-10 16:10:06 -0500

    button handlers added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
         <Fragment>
            <header className={classes.header}>
                <h1>ReactMeals</h1>
-               <HeaderCartButton/>
+               <HeaderCartButton onClick={props.onShowCart}/>
            </header>
            <div className={classes['main-header']}>
                <img src={mealsImage} alt='A table full of food' />
