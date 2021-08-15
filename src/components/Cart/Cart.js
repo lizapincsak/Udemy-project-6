2fcd6fa2fc7459ad321afa3fd4c988b4bd983813
lizapincsak <liza.pincsak@gmail.com>
2021-08-15 17:52:11 -0500

    adding items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 
 import classes from './Cart.module.css';
+import CartItem from './CartItem';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
 
@@ -11,10 +12,20 @@ const Cart = props => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
+    const cartItemRemoveHandler = id => {};
+
+    const cartItemAddHandler = item => {};
+
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map(item => (
-                <li>{item.name}</li>
+                <CartItem 
+                    key={item.id} 
+                    name={item.name} 
+                    amount={item.amount} 
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}/>
             ))}
         </ul>
     );
